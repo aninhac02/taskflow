48899c8b8a0eb441603b4822cf3d2c78afd1ac3b
Ana Caroline Vilela <anacarolinevilela0@gmail.com>
2023-04-17 18:02:29 -0300

    first commit

---
+++ src/test/java/com/api/taskflow/TaskflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.taskflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
